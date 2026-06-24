de24b5e072ef97bf46a5f6dff54739f47c99728b
Bored-Leaf <zachary1.voce@gmail.com>
2026-06-24 17:13:20 +1000

    added constructor

--- include/AppState.h
+++ include/AppState.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <atomic>
+#include <memory>
 #include <mutex>
 
 #include "shader.h"
 #include "CircularBuffer.h"
+#include "WAVReader.h"
 
 struct Button {
     float leftX{};
@@ -16,6 +18,8 @@ struct Button {
 };
 
 struct AppState {
+    AppState() : buffer(131072){}
+
     bool isPlaying{false};
     bool shouldLoop{false};
 
@@ -24,7 +28,9 @@ struct AppState {
     glm::mat4 uiProjection{1.0F};
     std::unique_ptr<Shader> UIShader;
 
-    CircularBuffer<float> buffer();
+    std::unique_ptr<WAVReader> WAVFile;
+
+    CircularBuffer<float> buffer;
     std::atomic<bool> running;
     std::mutex mtx;
 };
